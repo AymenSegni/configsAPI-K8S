7508a2c8b101396ac4804a8320b73c1be7bc709a
asegni <segniaymen1@gmail.com>
2020-03-08 02:49:24 +0100

    render deployment files

---
+++ scripts/env-for-local-dev.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+SERVE_PORT=5000
+export \
+  SERVE_PORT
+
+echo " set env var: SERVE_PORT="$SERVE_PORT
--- scripts/render-deployment-files.sh
+++ scripts/render-deployment-files.sh
@@ -1,1 +1,48 @@
+#!/bin/bash
+set -euo pipefail
 
+function ensureVariables {
+  # search for occurences of ${SOME_THING} in deployment yaml files
+  local varnames=$(grep -r '${[A-Z0-9_]\+}' -o -h deployment/ | sort | uniq | cut -f2 -d { | cut -f1 -d })
+  echo "Checking environment variables"
+  for varname in $varnames; do
+    if [[ -z ${!varname+x} ]]; then
+      echo "Environment variable $varname is not set, aborting."
+      return 1
+    elif [[ -z ${!varname} ]]; then
+      echo "Environment variable $varname is empty, aborting."
+      return 1
+    else
+      echo "Environment variable $varname is set."
+    fi
+  done
+}
+
+ensureVariables
+
+echo -n "Cleaning up ... "
+for d in rendered ; do
+  rm -rf ./${d}/ && mkdir -p ./${d}/
+done
+echo "done"
+
+echo -n "Rendering deployment manifests ... "
+for f in ./deployment/*.yaml
+do
+  filename=$(basename "${f}")
+  if [[ "${filename}" != "${filename#_prerendered-}" ]]
+  then
+    # prerendered files are not to be templated/rendered by envsubst
+    cp "${f}" "./rendered/${filename#_prerendered-}"
+  else
+    envsubst < "${f}" > "./rendered/${filename}"
+  fi
+done
+echo "done"
+
+echo "next step .."
+echo "Apply the Deployment manifests with 'kubectl apply -f ./rendered'"
+
+kubectl apply -f ./rendered
+echo "App deployed"
+echo "Done"
